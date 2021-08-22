dc51197fa596f23732abed58f0680b5d0753dd9d
Jens H <jens_hu@6nt.de>
2021-08-22 15:01:03 +0200

    Completed basic Tests for set Function

--- test/set.ts
+++ test/set.ts
@@ -1,16 +1,33 @@
 import set from "../src/set";
+import {PermObject} from "../src/interfaces/permObject";
+import * as assert from "assert";
 
 describe.skip("Set permissions to obj", () => {
+
+  let data: PermObject;
+
+  beforeEach('Data preparation', () => {
+    data = {test: true}
+  })
+
   it("save new true value to obj", () => {
-    set("test2", true, {test: true});
+    let res = set("test2", true, data);
+    assert.strictEqual(data, {test: true, test2: true}, 'Verify Permission Object Data');
+    assert.strictEqual(res, true, 'Marked as new Entry')
   });
   it("save new false value to obj", () => {
-    set("test2", false, {test: true});
+    let res = set("test2", false, data);
+    assert.strictEqual(data, {test: true, test2: false}, 'Verify Permission Object Data');
+    assert.strictEqual(res, true, 'Marked as new Entry')
   });
   it("save new nested true value to obj", () => {
-    set("test2.test3", true, {test: true});
+    let res = set("test2.test3", true, data);
+    assert.strictEqual(data, {test: true, test2: {test3: true}}, 'Verify Permission Object Data');
+    assert.strictEqual(res, true, 'Marked as new Entry')
   });
-  it("save new nested true value to obj", () => {
-    set("test2.test3", false, {test: true});
+  it("save new nested false value to obj", () => {
+    let res = set("test2.test3", false, data);
+    assert.strictEqual(data, {test: true, test2: {test3: false}}, 'Verify Permission Object Data');
+    assert.strictEqual(res, true, 'Marked as new Entry')
   });
 })
