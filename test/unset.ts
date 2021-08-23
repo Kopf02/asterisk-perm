176161102a0435b5fcb399a05b6f332be5c8bf4c
Nico Wagner <info@ni-wa.de>
2021-08-23 22:25:28 +0200

    added tests for unset function [ci skip]

---
+++ test/unset.ts
@@ -0,0 +1,83 @@
+import {PermObject} from "../src/interfaces/permObject";
+import unset from "../src/unset";
+import assert from "assert";
+
+describe("Unset permissions to obj", () => {
+
+  describe('Unset existing values', () => {
+    let data: PermObject;
+
+    beforeEach('Data preparation', () => {
+      data = {test: {_: true, test2: {test3: false}, test9: false}};
+    });
+    it("remove root value from deep nested obj", () => {
+      let res = unset("test", data);
+      assert.deepStrictEqual(data, {}, 'Verify Permission Object Data');
+      assert.strictEqual(res, false, 'Element should exist and deleted');
+    });
+    it("remove root.root value from deep nested obj", () => {
+      let res = unset("test.test", data);
+      assert.deepStrictEqual(data, {test: {test2: {test3: false}, test9: false}}, 'Verify Permission Object Data');
+      assert.strictEqual(res, false, 'Element should not exist');
+    });
+    it("remove existing value from deep nested obj", () => {
+      let res = unset("test.test2.test3", data);
+      assert.deepStrictEqual(data, {test: {_: true, test9: false}}, 'Verify Permission Object Data');
+      assert.strictEqual(res, true, 'Element should exist and deleted');
+    });
+
+  });
+  describe("Unset non existing values", () => {
+    let data: PermObject;
+
+    beforeEach('Data preparation', () => {
+      data = {test: {_: true, test2: {test3: false}, test9: false}};
+    });
+    it("remove non existing root value from obj", () => {
+      let res = unset("test.test2", data);
+      assert.deepStrictEqual(data, {
+        test: {
+          _: true,
+          test2: {test3: false},
+          test9: false
+        }
+      }, 'Verify Permission Object Data');
+      assert.strictEqual(res, false, 'Element should not exist');
+    });
+    it("remove non existing root value from deep nested obj", () => {
+      let res = unset("test2", data);
+      assert.deepStrictEqual(data, {
+        test: {
+          _: true,
+          test2: {test3: false},
+          test9: false
+        }
+      }, 'Verify Permission Object Data');
+      assert.strictEqual(res, false, 'Element should not exist');
+    });
+    it("remove non existing value from obj", () => {
+      let res = unset("test2.test3", data);
+      assert.deepStrictEqual(data, {
+        test: {
+          _: true,
+          test2: {test3: false},
+          test9: false
+        }
+      }, 'Verify Permission Object Data');
+      assert.strictEqual(res, false, 'Element should not exist');
+    });
+  });
+  describe("Delete * permission", () => {
+    let data: PermObject;
+
+    beforeEach('Data preparation', () => {
+      data = {test: {_: true, test2: {"*": false}, test9: false}};
+    });
+
+    it("remove star value from deep nested obj", () => {
+      let res = unset("test.test2.*", data);
+      assert.deepStrictEqual(data, {test: {_: true, test9: false}}, 'Verify Permission Object Data');
+      assert.strictEqual(res, true, 'Element should exist and deleted');
+    });
+  });
+});
